90b90b8da28ac4ccc18458b902bf8c8ba01ed275
Jairo Saul <JairoProDev@gmail.com>
2023-12-20 23:43:11 -0500

    Refactor ad creation API endpoint

--- pages/api/ads/create.ts
+++ pages/api/ads/create.ts
@@ -8,16 +8,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === 'POST') {
         const { title, description, imageUrl, userId, price, category, location, contactInfo } = req.body;
 
-        // Check if all fields are filled
-        if (!title || !description || !imageUrl || !userId || !price || !category || !location || !contactInfo) {
-            return res.status(400).json({ error: 'All fields are required.' });
+        // Check if title and description are filled
+        if (!title || !description) {
+            return res.status(400).json({ error: 'Title and description are required.' });
         }
 
         // Create new ad
         const newAd = new Ad({ title, description, imageUrl, userId, price, category, location, contactInfo });
         await newAd.save();
 
-        res.status(200).json({ message: 'Ad created successfully.' });
+        res.status(201).json(newAd);
     } else {
         res.status(405).json({ error: 'Method not allowed.' });
     }
