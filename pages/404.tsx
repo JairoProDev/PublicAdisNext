cb9ef125c77821a71ef988517f54959adf84917f
Jairo Saul Salas Quiñones Pro-Dev <JairoProDev@gmail.com>
2023-12-25 23:55:32 -0500

    Fix bug in login functionality
    Update README with new instructions
    
    Refactor code for better performance
    
    Add new feature for user authentication
    
    Fix formatting issues in code
    
    Update dependencies to latest versions
    
    Remove unused variables
    
    Fix broken links in documentation
    
    Add error handling for edge cases
    
    Optimize database queries

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const NotFoundPage: NextPage = () => {
+    return (
+        <div>
+            <h1>404 - Página no encontrada</h1>
+            <p>Lo sentimos, la página que estás buscando no existe.</p>
+        </div>
+    );
+};
+
+export default NotFoundPage;
